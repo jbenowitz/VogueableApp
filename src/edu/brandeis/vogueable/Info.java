f8fb0c4713a2d619327a0b7dad2a6624121e9292
Violeta Soued <vsoued@brandeis.edu>
2011-06-15 20:42:16 -0400

    please

---
+++ src/edu/brandeis/vogueable/Info.java
@@ -0,0 +1,11 @@
+package edu.brandeis.vogueable;
+
+import android.app.Activity;
+import android.os.Bundle;
+public class Info extends Activity{
+	   
+	protected void onCreate(Bundle savedInstanceState) {
+	     super.onCreate(savedInstanceState);
+	     setContentView(R.layout.info);
+	}
+}
