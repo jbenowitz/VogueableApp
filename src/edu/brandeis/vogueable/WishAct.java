1ef8df415e8e43822477e292f6e8f5482fc637ed
Violeta Soued <vsoued@brandeis.edu>
2011-06-26 18:16:16 -0400

    hello wishlist

---
+++ src/edu/brandeis/vogueable/WishAct.java
@@ -0,0 +1,12 @@
+package edu.brandeis.vogueable;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class WishAct extends Activity{
+	protected void onCreate(Bundle savedInstanceState) {
+	      super.onCreate(savedInstanceState);
+	      setContentView(R.layout.wish);
+	   }
+}
