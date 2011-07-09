d949a568fbee0d854028842b3f882e5f0bf40156
Gaspar Obimba <kenyangas@gmail.com>
2011-07-09 08:17:19 -0400

    cleaner message

--- src/edu/brandeis/vogueable/Women.java
+++ src/edu/brandeis/vogueable/Women.java
@@ -3,15 +3,19 @@ package edu.brandeis.vogueable;
 
 
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 /**
- * This is the activity for the Women app.
+ * This is the main activity for the Men app.
+ * Asks the user to either log in or head into browsing
  * @author gaspar obimba
  *
  */
@@ -28,9 +32,7 @@ public class Women extends Activity implements  android.view.View.OnClickListene
 	      View nextButton = findViewById(R.id.browse_label);
 	      nextButton.setOnClickListener(this);
 	      View prevButton = findViewById(R.id.login_label);
-	      prevButton.setOnClickListener(this);
-
-//	     
+	      prevButton.setOnClickListener(this);  
 	   }
 	  
 	  
@@ -47,25 +49,33 @@ public class Women extends Activity implements  android.view.View.OnClickListene
 	      case R.id.browse_label :
 	    	  Intent i = new Intent(this, CategoryChooser.class);
 		         startActivity(i);
-		         break;
 		         
+		         break;
 	      
 	      case R.id.login_label:
-	    	  AlertDialog.Builder builder = new AlertDialog.Builder(this);
-	    	  builder.setMessage("Are you sure you want to login?")
-	    	         .setCancelable(false)
-	    	         .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-	    	             public void onClick(DialogInterface dialog, int id) {
-	    	                // Men.this.finish();
-	    	            	
-	    	             }
-	    	         })
-	    	         .setNegativeButton("No", new DialogInterface.OnClickListener() {
-	    	             public void onClick(DialogInterface dialog, int id) {
-	    	                  dialog.cancel();
-	    	             }
-	    	         }).show();
-	         break;
+	    	  final AccountManager manager = AccountManager.get(this);
+		      final Account[] accounts = manager.getAccounts();
+		      if (accounts.length >=1){
+		    	  User user = new User(accounts[0].name);
+		    	  
+		    	  AlertDialog.Builder welcome = new AlertDialog.Builder(this);
+		    	  	welcome.setIcon(R.drawable.logobright);
+		    	  	welcome.setTitle(" ");
+		    	  	welcome.setMessage("Welcome, "+user.name);
+		    	  	welcome.setPositiveButton("Go!",new DialogInterface.OnClickListener() {
+			    	             public void onClick(DialogInterface dialog, int id) {
+			    			         startActivity(new Intent(Women.this, CategoryChooser.class));
+			    	            	 dialog.cancel();
+			    	             }
+			    	         	})
+			    	         .show();
+		    	  	//Need to implement method to update server with user info
+		    	  	
+		      } else {
+		    	  Toast.makeText(Women.this, "No account registered", Toast.LENGTH_SHORT).show();
+		      } 
+	    	 
+		      	break; 
 	      }
 	   }
 }
